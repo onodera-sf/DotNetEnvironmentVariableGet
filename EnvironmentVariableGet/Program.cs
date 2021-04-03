df40038530f159b0d252d9b8597de2a19a779fcf
onodera_sf <sf_yo_service_yellow@outlook.jp>
2021-04-03 10:46:02 +0900

    プロジェクト ファイルを追加します。

---
+++ EnvironmentVariableGet/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace EnvironmentVariableGet
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.WriteLine("■環境変数");
+
+			Console.WriteLine($"OS         = {Environment.GetEnvironmentVariable("OS")}");
+			Console.WriteLine($"PATHEXT    = {Environment.GetEnvironmentVariable("PATHEXT")}");
+			Console.WriteLine($"TEST_VALUE = {Environment.GetEnvironmentVariable("TEST_VALUE")}");
+
+			Console.WriteLine("いずれかのキーを押して終了してください。");
+			Console.ReadKey();
+		}
+	}
+}
---
+++ EnvironmentVariableGet/Start.bat
@@ -0,0 +1,6 @@
+@echo off
+rem 環境変数をセット
+set TEST_VALUE=セットした値
+
+rem プログラム起動
+EnvironmentVariableGet.exe
